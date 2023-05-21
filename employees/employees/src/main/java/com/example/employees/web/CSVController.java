b286115b659ed139bb7b38988b1bfffd5974248e
Stefan-ys <84866588+Stefan-ys@users.noreply.github.com>
2023-05-21 18:46:22 +0300

    Add files

---
+++ employees/employees/src/main/java/com/example/employees/web/CSVController.java
@@ -0,0 +1,35 @@
+package com.example.employees.web;
+
+import com.example.employees.entities.Pair;
+import com.example.employees.service.CSVService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.util.List;
+
+@RestController
+public class CSVController {
+    private final CSVService csvService;
+
+    public CSVController(CSVService csvService) {
+        this.csvService = csvService;
+    }
+
+
+
+    @PostMapping("/process-csv")
+    public ResponseEntity<List<Pair>> processCSV(@RequestParam("file") MultipartFile file) {
+        try {
+            List<Pair> result = csvService.processCSV(file);
+
+            return ResponseEntity.ok(result);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+    }
+}
