86e0fe5812ad78aace16b1d5ff5574fdc390c562
Stefan-ys <84866588+Stefan-ys@users.noreply.github.com>
2023-05-28 16:02:25 +0300

    Update EmployeesByProjectRepository.java

--- employees/src/main/java/com/example/employees/repositories/EmployeesByProjectRepository.java
+++ employees/src/main/java/com/example/employees/repositories/EmployeesByProjectRepository.java
@@ -10,6 +10,7 @@ import java.util.Map;
 
 
 public class EmployeesByProjectRepository {
+    
     private final Map<Integer, List<Employee>> projects;
 
     public EmployeesByProjectRepository() {
